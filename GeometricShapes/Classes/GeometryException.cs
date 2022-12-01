4a96395c405953efcc3701ddf4d782c578a1f18c
Purpel Lizrd <vladislav.nekrasov123@gmail.com>
2022-12-01 19:09:20 +0300

    Add project files.

---
+++ GeometricShapes/Classes/GeometryException.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometricShapes.Classes
+{
+    [Serializable]
+    public class GeometryException : Exception
+    {
+        public Int32[]? Parameters
+        {
+            get;
+            private set;
+        }
+        public GeometryException() : base()
+        {
+
+        }
+
+        public GeometryException(string message) : base(message)
+        {
+
+        }
+
+        public GeometryException(string message, Int32[] parameters) : base(message)
+        {
+            Parameters = parameters;
+        }
+
+        public GeometryException(string message, Exception inner) : base(message, inner)
+        {
+
+        }
+    }
+}
---
+++ GeometricShapes/Classes/Shapes.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    public class Triangle
+    {
+        public Triangle()
+        {
+
+        }
+    }
+
+    public class Quadrangle
+    {
+        public Quadrangle()
+        {
+
+        }
+    }
+
+    public class Circle
+    {
+        public Circle()
+        {
+
+        }
+    }
+}
